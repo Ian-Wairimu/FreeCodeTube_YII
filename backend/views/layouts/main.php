5445e276a669764698980a4106f9e98597ba1c36
Ian Wairimu <ianwairimu4@gmail.com>
2023-09-21 11:05:52 +0300

    FreeCodeTube: implemented user models the backend view and video upload model

---
+++ backend/views/layouts/main.php
@@ -0,0 +1,59 @@
+<?php
+
+/** @var \yii\web\View $this */
+/** @var string $content */
+
+use backend\assets\AppAsset;
+use common\widgets\Alert;
+use yii\bootstrap5\Breadcrumbs;
+use yii\bootstrap5\Html;
+use yii\bootstrap5\Nav;
+use yii\bootstrap5\NavBar;
+
+AppAsset::register($this);
+$this->beginContent('@backend/views/layouts/base.php');
+?>
+<header>
+    <?php
+    NavBar::begin([
+        'brandLabel' => Yii::$app->name,
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar navbar-expand-lg navbar-light bg-light shadow-sm static-top',
+        ],
+    ]);
+    $menuItems = [
+        ['label' => 'Create', 'url' => ['/site/index']],
+    ];
+    if (Yii::$app->user->isGuest) {
+        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+    } 
+    else {
+        $menuItems[] = [
+            'label' => 'Logout ('.Yii::$app->user->identity->username.')',
+            'url' => ['/site/logout'],
+            'linkOptions' => [
+                'data-method' => 'post'
+            ]
+        ];
+    }
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav ms-auto'],
+        'items' => $menuItems,
+    ]);
+    if (Yii::$app->user->isGuest) {
+        echo Html::tag('div',Html::a('Login',['/site/login'],['class' => ['btn btn-link login text-decoration-none']]),['class' => ['d-flex']]);
+    }
+    NavBar::end();
+    ?>
+</header>
+
+<main role="main" class="d-flex">
+    <?php echo $this->render('_sidebar')?>
+    <div class="content-wrapper p-3">
+        <?= Alert::widget() ?>
+        <?= $content ?>
+    </div>
+</main>
+
+<?php $this->endContent() ?>
